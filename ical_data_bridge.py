6858d83270dd9b3be337915cca514d2551a2f4db
Wang Tai <i@wangtai.me>
2014-02-24 14:46:05 +0800

    add 增加 ical_data_bridge文件 把网上的数据处理成ical格式

---
+++ ical_data_bridge.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Last modified: Wang Tai (i@wangtai.me)
+
+"""
+把网上的数据处理成ical格式
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+BEGIN:VEVENT
+UID:uid1@example.com
+DTSTAMP:19970714T170000Z
+ORGANIZER;CN=John Doe:MAILTO:john.doe@example.com
+DTSTART:19970714T170000Z
+DTEND:19970715T035959Z
+SUMMARY:Bastille Day Party
+END:VEVENT
+END:VCALENDAR
+"""
+
+__revision__ = '0.1'
+

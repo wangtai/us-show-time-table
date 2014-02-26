f48a104562e13d7cebcd2404b1a4748cab5616f5
Wang Tai <i@wangtai.me>
2014-02-26 12:03:29 +0800

    update 添加time_table.ics域名

--- 1/showics/views.py
+++ 1/showics/views.py
@@ -28,3 +28,13 @@ def fetch_yyets_data(request):
     time_table = time_table_data_source.read_time_table(i_year, i_this_month)
     ical_data = ical_data_bridge.ical_data(i_year, i_this_month, time_table)
     return HttpResponse(ical_data)
+
+
+@to_url('^time_table.ics')
+def fetch_yyets_data(request):
+    i_year = int(time.strftime('%Y'))
+    i_this_month = int(time.strftime('%m'))
+    time_table = time_table_data_source.read_time_table(i_year, i_this_month)
+    ical_data = ical_data_bridge.ical_data(i_year, i_this_month, time_table)
+    #Content-Type:text/calendar; charset=UTF-8
+    return HttpResponse(content=ical_data, content_type='Content-Type:text/calendar; charset=UTF-8')

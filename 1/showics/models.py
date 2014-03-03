577f2eab8dcf1d6f192dcc911284d57884f2f979
Wang Tai <i@wangtai.me>
2014-03-03 14:33:16 +0800

    update 设计show_table_ics 表

--- 1/showics/models.py
+++ 1/showics/models.py
@@ -7,3 +7,17 @@
 
 __revision__ = '0.1'
 
+from django.db import models
+
+
+class ShowTableIcs(models.Model):
+    # uid
+    uid = models.CharField(max_length=255, unique=True, primary_key=True)
+    # title
+    title = models.CharField(max_length=255, null=False)
+    # description
+    description = models.CharField(max_length=255)
+    # date
+    date = models.DateField()
+    class Meta(object):
+        db_table = 'show_table_ics'
